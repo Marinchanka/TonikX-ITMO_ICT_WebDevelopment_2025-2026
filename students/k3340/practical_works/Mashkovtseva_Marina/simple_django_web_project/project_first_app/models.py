80a75402f65a00b88faa565d58d66b3c0909157d
marinchanka <marinchanka@yandex.ru>
2025-12-05 03:21:33 +0300

    Добавлены материалы по 3 ЛР

--- students/k3340/practical_works/Mashkovtseva_Marina/simple_django_web_project/project_first_app/models.py
+++ students/k3340/practical_works/Mashkovtseva_Marina/simple_django_web_project/project_first_app/models.py
@@ -18,15 +18,15 @@ class Car(models.Model):
 
 class Ownership(models.Model):
     id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, null=True, blank=True)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, null=True, blank=True, related_name='ownerships')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True, related_name='ownerships')
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(null=True, blank=True)
 
 
 class DriverLicense(models.Model):
     id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='licenses')
     license_number = models.CharField(max_length=10)
     license_type = models.CharField(max_length=10)
     issue_date = models.DateTimeField()
